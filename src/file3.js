d4618d0ee6ffc653d01f9f83d779b67a5a253369
ahelman <ahelman@yseop.com>
2019-03-08 15:11:03 +0100

    multiply

--- src/file3.js
+++ src/file3.js
@@ -6,6 +6,10 @@ export function calculatePower(number, power) {
 	return Math.pow(number, power);
 }
 
+export function multiply(number, power) {
+	
+	return Math.pow(number, power);
+}
 export function divide(a, b) {
 	
 	return Math.pow(a, b);
